4be6213267dcce35293092e0a1c417a1bfc04ae6
Gary Gao <>
2025-06-29 22:54:34 -0400

    w new directory

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+// src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './app.css'
+
+console.log('main.tsx is loading...');
+
+const rootElement = document.getElementById('root');
+console.log('Root element:', rootElement);
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} else {
+  console.error('Root element not found!');
+}
